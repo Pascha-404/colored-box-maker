773562b42b84dc1ebd5a889737644cf76100ea94
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-31 23:56:26 +0200

    correct linking of maincomponent

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import BoxList from './BoxList';
 
 function App() {
-  return (
-    <div className="App">
-      <BoxList />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<BoxList />
+		</div>
+	);
 }
 
 export default App;
