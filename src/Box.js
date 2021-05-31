bf4969a6c3a9a882381a95f52f14d7effa9c6035
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-05-31 23:56:02 +0200

    logic for Box

--- src/Box.js
+++ src/Box.js
@@ -2,8 +2,28 @@ import React, { Component } from 'react';
 import './Box.css';
 
 class Box extends Component {
+	constructor(props) {
+		super();
+		this.handleDelete = this.handleDelete.bind(this);
+	}
+
+	handleDelete(evt) {
+		this.props.deleteBox(this.props.id);
+	}
+
 	render() {
-		return <h2>Box</h2>;
+		const divStyle = {
+			WebkitTransition: 'all',
+			msTransition: 'all',
+			width: this.props.width,
+			height: this.props.height,
+			backgroundColor: this.props.backgroundColor,
+		};
+		return (
+			<div style={divStyle} className='Box'>
+				<button onClick={this.handleDelete}>&#10006;</button>
+			</div>
+		);
 	}
 }
 
